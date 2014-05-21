a7803eca0f966f4212a9749303d62b9c9ade88f8
Célio Júnior <celiomsj@gmail.com>
2014-05-21 15:18:37 -0300

    Assignment resolution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+# Coursera
+# R Programming
+# Programming Assignment 2: Lexical Scoping
+#
+# The objetive of this R code is to create a function that caches the value
+# of the inverse of a matrix.
+# 
+# Example usage is as follows:
+# > myMatrix <- matrix(rnorm(9), 3, 3)
+# > myMatrixCache <- makeCacheMatrix(myMatrix)
+# > cacheSolve(myMatrixCache)
+# > cacheSolve(myMatrixCache)
+#
+# The second cacheSolve call uses the cached value.
+#
 
-## Write a short comment describing this function
+
+# makeCacheMatrix returns a special object that stores a matrix and a
+# cached measure
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setcache <- function(cache) m <<- cache
+  getcache <- function() m
+  list(set = set, get = get,
+       setcache = setcache,
+       getcache = getcache)
+  
 }
 
 
-## Write a short comment describing this function
+# cacheSolve checks if there is a cached value for x, a object created by
+# makeCacheMatrix. If it doesn't exist, computes x inverse, caches it and
+# return its value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getcache()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setcache(m)
+  m
 }
